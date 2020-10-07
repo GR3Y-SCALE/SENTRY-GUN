c56b56175cb735e6b3490a3cd3f7e92e30846498
GREY <62605251+GR3Y-SCALE@users.noreply.github.com>
2020-10-07 21:34:19 +0930

    Update arduinoLAZER-TURRET-pot.ino
    better explanation of how to set up

--- arduinoLAZER-TURRET-pot.ino
+++ arduinoLAZER-TURRET-pot.ino
@@ -1,3 +1,18 @@
+/*
+ * This Sketch was created by GR3Y, feel free to modify in anyway.
+ * 
+ * This Sketch is used with the laserturret or any following turret projects.
+ * 
+ * Controls servos on pins 4 and 5 with angles specified by data in from serial.
+ * 
+ * Simple explanation, the wiper of first pot is put on digital pin 4, same for the second pot (both pots are 10K) the other pins on the pot are either put to ground (-) or VCC (5V).
+ * 
+ * If an axis is reversed then make swap the 0 value infront of 180 in ValX or valY a negative E.G  
+ *
+ *  valX = map(valX, 0, 1023, 0, 180); if x axis is inverted then swap the 0 value with 180
+ *
+ *  valX = map(valX, 0, 1023, 180, 0);
+ */
 #include <Servo.h>
 
 Servo servoX;  // create servo object to control a servo
@@ -14,11 +29,13 @@ void setup() {
 }
 
 void loop() {
+  //for X axis
   valX = analogRead(potXpin);            // reads the value of the potentiometer (value between 0 and 1023)
   valX = map(valX, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
   servoX.write(valX);                  // sets the servo position according to the scaled value
 
-  valY = analogRead(potYpin);           
+  //for Y axis
+  valY = analogRead(potYpin);     
   valY = map(valY, 1023, 0, 180, 0);     
   servoY.write(valY);                         
 }
